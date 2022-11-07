063585c77b1685fcf9258259fe5017f3c157d629
Balachandar Shanmugam <90779048+balachandar0811@users.noreply.github.com>
2022-11-07 12:39:29 -0600

    Update streamlit_app.py
    Added header ,text & changed title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,10 @@
 import streamlit
-streamlit.title('My  learning on Snowflake Data Application Builders Workshop')
+
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.title('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
